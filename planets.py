898afdd75af778953bb9193485bf9d7873f461e4
salnigro <salnigro627@gmail.com>
2020-02-25 08:29:58 -0600

    planet

---
+++ planets.py
@@ -0,0 +1,18 @@
+class planet(object):
+
+
+    def __init__(self,obp,name,grav,mass):
+        self.name = name
+        self.obp = obp
+        self.grav = grav
+        self.mass = mass
+
+    def getobp():
+        return obp
+    def getname():
+        return name
+    def getgrav():
+        return grav
+    def getmass():
+        return mass
+    
